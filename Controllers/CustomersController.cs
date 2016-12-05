3f00374629ec932e55a8ca2348ec3d7eac6e91e1
Dushyanth91 <dushyanthmunna91@gmail.com>
2016-12-05 17:54:41 -0600

    First Vidly Commit
    first commit :)

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+     
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers().SingleOrDefault(x => x.Id == id);
+
+            if (customers == null)
+                return HttpNotFound();
+
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Name = "Dushyanth Reddy", Id = 1},
+                new Customer {Name = "Abhishek Tadisina", Id = 2}
+            };
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie {Name = "Shrek!"},
+                new Movie {Name = "Wall E"}
+            };
+        }
+    }
+}
